9e9026d8f10d05921b6c0f0d9bf7358827004fe0
20120392 <ptbt1092@gmail.com>
2024-04-10 20:33:19 +0700

    create UI for page Account

--- src/components/myaccount/myaccount.js
+++ src/components/myaccount/myaccount.js
@@ -1,46 +1,46 @@
-import { useDispatch } from "react-redux";
-import { NavLink, useLocation } from "react-router-dom";
+import React, { useState } from "react";
 
-import { logoutAccount } from "../../store/user";
+import { ReactComponent as EditIcon } from "../../assets/svg/edit_icon.svg";
+
+import "./myaccount.css";
 
 function MyAccount() {
-  const currentPage = useLocation().pathname;
-  const dispatch = useDispatch();
+  // Example state for edit mode - replace with actual implementation
+  const [, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    // Set the state to true, showing the edit form, for example
+    setIsEditing(true);
+  };
+
   return (
-    <div>
-      <div>Tài khoản của tôi</div>
-      <div
-        className={
-          currentPage === "/account/update-profile"
-            ? "nav-link active"
-            : "nav-link"
-        }
-      >
-        <NavLink to="profile" className="account-navlink">
-          Chỉnh sửa
-        </NavLink>
+    <div className="account-container">
+      <div className="account-info-header">Tài khoản</div>
+      <div className="account-info">
+        <div className="info-section">
+          <div className="info-name">Duy An Lê</div>
+          <div className="info-email">ptbt1092@gmail.com</div>
+          <div className="info-phone">+84354532051</div>
+        </div>
+        <button onClick={handleEditClick} className="info-action" type="button">
+          <EditIcon />
+          <div className="info-action-title">Chỉnh sửa</div>
+        </button>
       </div>
-      <div
-        className={
-          currentPage === "/account/deletion_confirmation"
-            ? "nav-link active"
-            : "nav-link"
-        }
-      >
-        <NavLink to="deletion_confirmation" className="account-navlink">
-          Xoá tài khoản
-        </NavLink>
+      <div className="account-actions">
+        <button className="action-button" type="button">
+          Cập nhật lại mật khẩu
+        </button>
+        <button className="action-button" type="button">
+          Xóa tài khoản
+        </button>
+        <button className="action-button" type="button">
+          Đăng xuất
+        </button>
       </div>
-      <button
-        className="logout"
-        type="button"
-        onClick={() => {
-          localStorage.removeItem("access-token");
-          dispatch(logoutAccount());
-        }}
-      >
-        Đăng xuất
-      </button>
+      <a href="/" className="back-home">
+        Trở về trang chủ
+      </a>
     </div>
   );
 }
